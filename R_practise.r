81f4dc78d3f3718d88ad026ca9e2f0221dd77300
Delaxshana Santhiravathanan <48511708+Delaxshana@users.noreply.github.com>
2020-05-08 23:25:55 +0530

    Add files via upload

---
+++ R_practise.r
@@ -0,0 +1,33 @@
+# Print variable x
+x <- 42
+x
+
+# Numerical
+vec_num <- c(1, 10, 49)
+vec_num
+
+# Character 
+vec_chr <- c("a", "b", "c")
+vec_chr
+
+# Create the vectors
+vect_1 <- c(1, 3, 5)
+vect_2 <- c(2, 4, 6)
+# Take the sum of A_vector and B_vector
+sum_vect <- vect_1 + vect_2
+# Print out total_vector
+sum_vect
+
+# A multiplication
+3*10
+# Construct a matrix with 5 rows that contain the numbers 1 up to 10 and byrow =  TRUE 
+matrix_a <-matrix(1:10, byrow = TRUE, nrow = 5)
+matrix_a
+
+# concatenate c(1:5) to the matrix_a
+matrix_a1 <- cbind(matrix_a, c(1:5))
+# Check the dimension
+dim(matrix_a1)
+
+
+
